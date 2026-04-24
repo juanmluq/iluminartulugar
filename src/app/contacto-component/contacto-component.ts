86c95a2beb8f6eb48ece81f675cc9abf63a08ea1
juanmluq <soyluque@hotmail.com>
2026-04-24 02:12:45 -0300

    update Swal

--- src/app/contacto-component/contacto-component.ts
+++ src/app/contacto-component/contacto-component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contacto-component',
@@ -31,11 +32,25 @@ export class ContactoComponent {
   enviar() {
 
     if (this.miFormulario.valid) {
-      alert('El sistema ha registrado su calificación y se ingresara si su número de cliente es valido.');
-      this.miFormulario.reset();
-            window.location.reload();   
+      // alert('El sistema ha registrado su calificación y se ingresara si su número de cliente es valido.');
+      Swal.fire({
+        title: 'Gracias por tu calificación!',
+        text: 'El sistema ingresara su calificación si su número de cliente es valido.',
+        icon: 'success',
+        confirmButtonText: 'Ok',
+        preConfirm: () => {
+          this.miFormulario.reset();
+          window.location.reload();   
+        }
+        })
     } else{
-          alert('Error. Complete todos los campos!');
+          // alert('Error. Complete todos los campos!');
+           Swal.fire({
+            title: 'Error!',
+            text: 'Complete todos los campos!',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+            })
 
     }
 
